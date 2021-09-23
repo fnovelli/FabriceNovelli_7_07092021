d96784daa30f5fa1c71d522e1354b480ee90dafd
Fab <f.novelli@hotmail.fr>
2021-09-23 17:16:44 +0200

    Added Password validator package + User controller for connection.

--- Backend/MySql.js
+++ Backend/MySql.js
@@ -1,25 +1,26 @@
 
 var mysql = require('mysql');
+const app = require('./app');
 const schema = `CREATE DATABASE IF NOT EXISTS ${process.env.SQL_DB}`;
 
 
-const con = mysql.createConnection({
+const db = mysql.createConnection({
 
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASS,
 });
 
+// we try to connect the user, then we create the database.
+db.connect(function(err) {
 
-con.connect(function(err) {
-  
    if (err) {
      throw err; 
     }
 
     console.log("SQL User connected");
 
-    con.query(schema, function (err, result) {
+    db.query(schema, function (err, result) {
 
       if (err) {
         throw err; 
---
+++ Backend/controllers/user.js
@@ -0,0 +1,65 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+var passwordValidator = require('password-validator');
+
+//limit inject attack and force user to have strong password.
+var schema = new passwordValidator();
+let secret = process.env.JWT_SECRET;
+
+schema
+.is().min(8)                                    
+.is().max(30)                                 
+.has().uppercase(1)                              
+.has().lowercase()                              
+.has().digits(1)
+.has().symbols(1)                                 
+.has().not().spaces();
+
+
+exports.signup = (req, res, next) => {
+
+    //check if password is strong enough
+  if (!schema.validate(req.body.password)) {
+    throw new error("Error! Password must be stronger. It must have at least one uppercase, one digit, one special character");
+  }
+
+  //crypt password 
+  bcrypt.hash(req.body.password, 10)
+  .then(hash =>{
+      const user = new User({
+          email: req.body.email,
+          password: hash
+      });
+      user.save()
+      .then(() => res.status(201).json({message: 'User created!'}))
+      .catch(error => res.status(400).json({error: 'Couldn\'t create user'}));
+  })
+  .catch(error => res.status(500).json({error}));
+};
+
+exports.login = (req, res, next) => {
+  
+  User.findOne({ email: req.body.email })
+  .then(user => {
+      if (!user) {
+          return res.Status(401).json( { error: 'User not found'});
+      }
+      bcrypt.compare(req.body.password, user.password)
+      .then(valid => {
+          if (!valid) {
+              return res.Status(401).json( { error: 'Incorrect password.'});
+          }
+          res.status(200).json({
+              userId: user._id,
+              token: jwt.sign(
+                { userId: user._id },
+                secret,
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+};
