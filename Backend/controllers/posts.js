0fc4147c671f33bd7ed4519cfecba072a40b3b40
fnovelli <f.novelli@hotmail.fr>
2021-11-04 16:54:26 +0100

    Added CRUD operation for Posts (work on postman)

--- Backend/controllers/posts.js
+++ Backend/controllers/posts.js
@@ -13,8 +13,69 @@ exports.createPost = (req, res) => {
       return res.status(201).send({ post: "Message posted!" });
   })
   .catch(error => res.status(400).json({ error }));
-
 };
+
+exports.getPost = (req, res) => {
+  
+  const id = req.params.id;
+
+  Posts.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Message with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving message with id=" + id
+      });
+    });
+};
+
+exports.getAllPosts = (req, res) => {
+
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  
+  Posts.findAll( { where: condition}).
+  then((data) => {
+    res.send(data);
+  })
+.catch((error) => {
+    console.log(error);
+});
+};
+
+exports.updatePost = (req, res) => {
+  try {
+
+    const id = req.params.id;
+    Posts.update( req.body, { where: { id: id } }); 
+    return res.status(200).json({ message: "Successfully updated post!" });
+    
+  } catch (error) {
+    return res.status(500).send({ error: "Error, couldn't update post!" });
+  }
+};
+
+exports.deletePost = (req, res) => {
+
+  try {
+    const id = req.params.id;
+    Posts.destroy({ where: { id: id } }); 
+    return res.status(200).json({ message: "Successfully deleted post!" });
+    
+  } catch (error) {
+    return res.status(500).send({ error: "Error, couldn't delete post!" });
+  }
+};
+
+
+
 
 async function createPostTable() {
   const post = Posts.create ({ 
--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -3,5 +3,9 @@ const postsCtrl = require("../controllers/posts");
 
 
 router.post("/", postsCtrl.createPost);
+router.get('/:id', postsCtrl.getPost);
+router.get('/', postsCtrl.getAllPosts);
+router.put('/:id', postsCtrl.updatePost);
+router.delete('/:id', postsCtrl.deletePost);
 
 module.exports = router;
